6ca7ca1be8cedbf45e469b037a3748b5653ea8e0
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-03-04 21:54:22 -0300

    add uso do filtro filtrar_por

--- usar_classes.py
+++ usar_classes.py
@@ -6,7 +6,7 @@ path = "./custo.csv"
 df_path = Processor(path)
 print(df_path.df)
 print("################")
-print(df_path.filtrar_por('custo', 10))
+print(df_path.filtrar_por(['custo'], [10]))
 print("##################")
 df_path = df_path.tratando_nulos()
 print(df_path)
