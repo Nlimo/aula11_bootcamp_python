1888f7ced961fe155d245ece32afa80d1e03b0db
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-03-04 21:53:10 -0300

    add recursividade no metodo filtrar_por

--- classes.py
+++ classes.py
@@ -6,9 +6,23 @@ class Processor:
         self.df_filtrado = pd.DataFrame
         self.df = self.csv
 
-    def filtrar_por(self, atributo: str, valor: str):
-        self.df_filtrado = self.df[self.df[atributo] == valor]
-        return self.df_filtrado
+    ## receber um str str[]
+    def filtrar_por(self, colunas, atributos):
+        if len(colunas) != len(atributos):
+            raise ValueError("Não tem o mesmo número de colunas e atributos")
+        
+        if len(colunas) == 0:
+            return self.df
+        
+        coluna_atual = colunas[0]
+        atributo_atual = atributos[0]
+
+        df_filtrado = self.df[self.df[coluna_atual] == atributo_atual]
+
+        if len(colunas) == 1:
+            return df_filtrado
+        else:
+            return self.filtrar_por(colunas[1:], atributos[1:])
     
     def tratando_nulos(self):
         self.df = self.df.dropna(subset=['cidade', 'custo'], how='all')
